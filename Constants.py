a18c6a70033a21415b71fa5b356608afe402a77e
Laptop/Hans <hnsgunther@gmail.com>
2017-12-13 03:13:44 -0700

    Added a file to store constants where they can be accessed across files

---
+++ Constants.py
@@ -0,0 +1,16 @@
+# *****************************************************************************
+# CONSTANTS
+# *****************************************************************************
+
+# About data
+NUM_CHANNELS = 2
+ORIGINAL_SAMP_RATE_S = 44100
+
+# Downsampled data
+DOWNSAMPLE_FACTOR = 4
+SAMP_RATE_S = 44100//4 # Vals / s (Hz)
+SAMP_RATE_MS = SAMP_RATE_S / 1000 # vals / ms (kHz)
+
+# Chunks
+CHUNCK_SIZE_MS = 250 # Milliseconds, not megaseconds
+NUM_SAMPS_IN_CHUNCK = int(CHUNCK_SIZE_MS * SAMP_RATE_MS)
