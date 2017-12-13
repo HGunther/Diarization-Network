3269534734d2898df6bcb1b582bc90b7c1f005d7
Laptop/Hans <hnsgunther@gmail.com>
2017-12-13 06:30:32 -0700

    Removed two files that are no longer being used

--- diar_nn.py
+++
@@ -1,9 +0,0 @@
-from diar_nn_declare import *
-import tensorflow as tf
-
-def evaluate(chunk):
-    with tf.session() as sess:
-        # TODO restore network weights
-        chunk_freq = get_freqs(chunk)
-        chunk_val = Y_prob.eval(feed_dict={X: chunk, X_freq: chunk_freq})
-        return chunk_val
