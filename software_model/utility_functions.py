2e62c9cabe997ae60b597e3b594970cd4e24dea0
DeJoelson <webmaster1234567890@gmail.com>
2018-05-29 12:24:37 -0600

    Attempting to change all hard coded numbers to constant names

--- software_model/utility_functions.py
+++ software_model/utility_functions.py
@@ -9,7 +9,8 @@ def downsample(infile, outfile, downrate_factor):
     """Downsample an audio file"""
     rate, aud = wavfile.read(infile)
     daud = decimate(aud, downrate_factor, axis=0)
-    daudint = np.int16(daud / np.max(np.abs(daud)) * 32767)
+    int16_max = np.iinfo(np.dtype('int16')).max
+    daudint = np.int16(daud / np.max(np.abs(daud)) * int16_max)
     wavfile.write(outfile, rate // downrate_factor, daudint)
 
 
@@ -17,18 +18,21 @@ def downsample_inline(infile, downrate_factor):
     """Downsample an audio file"""
     _, aud = wavfile.read(infile)
     daud = decimate(aud, int(downrate_factor), axis=0)
-    return np.int16(daud / np.max(np.abs(daud)) * 32767)
+    int16_max = np.iinfo(np.dtype('int16')).max
+    return np.int16(daud / np.max(np.abs(daud)) * int16_max)
 
 
-def get_freqs(batch):
+def apply_fft(array_of_chunk_audio):
     """FFT"""
-    # Take FFT of each
-    for i in range(batch.shape[0]):
-        batch[i, :, 0, 0] = np.abs(fft(batch[i, :, 0, 0]))
-        batch[i, :, 1, 0] = np.abs(fft(batch[i, :, 1, 0]))
+    # Take FFT of each chunk in the array
+    for i in range(array_of_chunk_audio.shape[0]):
+        # Channel 1
+        array_of_chunk_audio[i, :, 0, 0] = np.abs(fft(array_of_chunk_audio[i, :, 0, 0]))
+        # Channel 2
+        array_of_chunk_audio[i, :, 1, 0] = np.abs(fft(array_of_chunk_audio[i, :, 1, 0]))
 
     # Real number symmetry of Fourier Transform
-    half_length = batch.shape[1] // 2
-    batch = batch[:, :half_length, :, :]
+    half_length = array_of_chunk_audio.shape[1] // 2
+    array_of_chunk_audio = array_of_chunk_audio[:, :half_length, :, :]
 
-    return batch
+    return array_of_chunk_audio
